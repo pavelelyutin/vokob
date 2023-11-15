b62dd4f633012b8e1acf857c65f774ae46603564
Елютин Павел Игоревич <Elyutin@ad.vokob.ru>
2023-11-15 16:24:05 +0300

    style input in form

--- js/script.js
+++ js/script.js
@@ -86,3 +86,82 @@ modalLogin.addEventListener('click', (e) => {
   e.currentTarget.classList.remove('open');
   // document.body.classList.remove('body-hidden');
 })
+
+
+// initMap();
+
+// async function initMap() {
+//     // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+//     await ymaps3.ready;
+
+//     const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker} = ymaps3;
+
+//     // Иницилиазируем карту
+//     const map = new YMap(
+//         // Передаём ссылку на HTMLElement контейнера
+//         document.getElementById('map'),
+
+//         // Передаём параметры инициализации карты
+//         {
+//             location: {
+//                 // Координаты центра карты
+//                 center: [39.195930, 51.672947],
+//                 // 51.672947, 39.195930
+//                 // Уровень масштабирования
+//                 zoom: 15
+//             }
+//         }
+//     );
+
+//     // Добавляем слой для отображения схематической карты
+//     map.addChild(new YMapDefaultSchemeLayer());
+//     map.addChild(new YMapDefaultFeaturesLayer());
+
+//     // Создайте DOM-элемент для содержимого маркера.
+// // Важно это сделать до инициализации маркера!
+// // Элемент можно создавать пустым. Добавить HTML-разметку внутрь можно после инициализации маркера.
+// const content = document.createElement('div');
+
+// // Инициализируйте маркер
+// const marker = new YMapMarker(
+//   {
+//     source: './img/lacation.svg',
+//     coordinates: [39.195930, 51.672947],
+//     draggable: false
+//   },
+//   content
+// );
+
+// // Добавьте маркер на карту
+// map.addChild(marker);
+
+// // Добавьте произвольную HTML-разметку внутрь содержимого маркера
+// content.innerHTML = '<h1>Этот заголовок можно перетаскивать</h1>';
+// }
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   // pagination: {
+//   //   el: '.swiper-pagination',
+//   // },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   slidesPerView: 4,
+//   mousewheel: {
+//     forceToAxis: true,
+//   },
+
+//   // And if we need scrollbar
+//   // scrollbar: {
+//   //   el: '.swiper-scrollbar',
+//   // },
+// });
